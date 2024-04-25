2bde52d36ed58e2303f7f73e4bcc5512bb4c4954
SamuelFullerCA <samuelfullerca@gmail.com>
2024-04-25 13:41:47 -0700

    updates

--- src/components/UserReviewee/index.jsx
+++ src/components/UserReviewee/index.jsx
@@ -6,11 +6,13 @@ function UserReviewee(props) {
         <section >
             <div className="columns-1 reviews">
                 <h2>Reviews About Me</h2>
-                {/* {props.reviews?.map((rev, i, j) =>
-                    <div className="reviewContainer">
-                        <p key={i}>{rev.review}</p>
+                {props.reviews?.map((rev, i) =>
+                    <div key={i} className="reviewContainer">
+                        <p>{rev.review}</p>
+                        <h6>Reviewd by: {rev.reviewer.username}</h6>
                     </div>
-                )} */}
+                    
+                )}
                 {/* <h6 key={j}>Reviewd by: {rev.reviewer.username}</h6> */}
             </div>
         </section>
