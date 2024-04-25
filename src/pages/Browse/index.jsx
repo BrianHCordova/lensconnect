6aaca09b74f15cc2447159e1e0de3eb64a88d55d
anduhrooo <yang.andrew7@gmail.com>
2024-04-25 02:07:37 -0700

    updates to frontend components

--- src/pages/Browse/index.jsx
+++ src/pages/Browse/index.jsx
@@ -1,11 +1,16 @@
+import Card from './card';
+
 function Browse() {
     
     // HTML
     return (
         <>
-            <h1>
-                BROWSE
-            </h1>
+            <h1>BROWSE</h1>
+            {/* each card is imported as passed imaged and title props, these are just... placeholders */}
+            <Card image="https://placebeard.it/640/480" title="is this a joe?"/>
+            <Card image="https://placebeard.it/640/480" title="is this a joe?"/>
+            <Card image="https://placebeard.it/640/480" title="is this a joe?"/>
+            <Card image="https://placebeard.it/640/480" title="is this a joe?"/>
         </>
 
     );
